e2fd65439e8cfdb3bb1364177b92f43ee2b64467
Herschdorfer <herschdorfer@gmail.com>
2018-03-04 17:30:42 +0100

    added bluetooth beacon

--- ESPExample.ino
+++ ESPExample.ino
@@ -33,135 +33,149 @@ SimpleBLE ble;
 String beaconMsg = "ESP00";
 int rec = 0;
 
-class BLEData {
-  private:
-    uint32_t  _Pressure;
-    int16_t   _Temperature;
-  public:
-    void FillData(double pTemp, double pPress) {
-      //use string to avoid rounding errors (else e.g. 1.30 could become 1.29)
-      String strTmp = String(pTemp * 100.0);
-      _Temperature = strTmp.toInt();
 
-      strTmp = String(pPress * 10.0);
-      _Pressure = strTmp.toInt();
-      Serial.println(_Pressure);
-    }
-    uint8_t* Temperature() {
-      return ((uint8_t*)&_Temperature);
-    }
 
-    uint8_t* Pressure() {
-      return ((uint8_t*)&_Pressure);
-    }
+class Data {
+private:
+	double  _Pressure;
+	double  _Temperature;
+
+	uint32_t _Pressure_int;
+	int16_t _Temperature_int;
+
+public:
+	void FillData(double pTemp, double pPress) {
+		this->_Pressure = pPress;
+		this->_Temperature = pTemp;
+
+		String temp = String(_Pressure * 10.0);
+		_Pressure_int = temp.toInt();
+
+		temp = String(_Temperature * 100.0);
+		_Temperature_int = temp.toInt();
+	}
+
+	String getTemperatureAsString() {
+		return toString(_Temperature);
+	}
+
+	String getPressureAsString() {
+		return toString(_Pressure);
+	}
+
+	uint8_t* Temperature() {
+		return ((uint8_t*)&_Temperature_int);
+	}
+
+	uint8_t* Pressure() {
+		return ((uint8_t*)&_Pressure_int);
+	}
+
+private:
+	String toString(double value)
+	{
+		return String(value);
+	}
+
 };
 
 
 class MyServerCallbacks : public BLEServerCallbacks {
-    bool _BLEClientConnected = false;
+	bool _BLEClientConnected = false;
 
-    void onConnect(BLEServer* pServer) {
-      _BLEClientConnected = true;
-    };
+	void onConnect(BLEServer* pServer) {
+		_BLEClientConnected = true;
+	};
 
-    void onDisconnect(BLEServer* pServer) {
-      _BLEClientConnected = false;
-    }
+	void onDisconnect(BLEServer* pServer) {
+		_BLEClientConnected = false;
+	}
 };
 
 void InitBLE() {
-  BLEDevice::init("ESP32-EE060");
+	BLEDevice::init("ESP32-EE060");
 
-  // Create the BLE Server
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
+	// Create the BLE Server
+	BLEServer *pServer = BLEDevice::createServer();
+	pServer->setCallbacks(new MyServerCallbacks());
 
-  // Create the BLE Service
-  BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x181A));
+	// Create the BLE Service
+	BLEService *pService = pServer->createService(BLEUUID((uint16_t)0x181A));
 
-  tempCharacteristic = pService->createCharacteristic(
-                         BLEUUID((uint16_t)TEMP_UUID),
-                         BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                       );
+	tempCharacteristic = pService->createCharacteristic(
+		BLEUUID((uint16_t)TEMP_UUID),
+		BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
+	);
 
-  pressureCharacteristic = pService->createCharacteristic(
-                             BLEUUID((uint16_t)PRESSURE_UUID),
-                             BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
-                           );
+	pressureCharacteristic = pService->createCharacteristic(
+		BLEUUID((uint16_t)PRESSURE_UUID),
+		BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY
+	);
 
-  pressureDescriptor.setValue("Pressure Pa");
-  pressureCharacteristic->addDescriptor(&pressureDescriptor);
-  pressureCharacteristic->addDescriptor(new BLE2902());
+	pressureDescriptor.setValue("Pressure Pa");
+	pressureCharacteristic->addDescriptor(&pressureDescriptor);
+	pressureCharacteristic->addDescriptor(new BLE2902());
 
-  tempDescriptor.setValue("Temperature -40-60°C");
-  tempCharacteristic->addDescriptor(&tempDescriptor);
-  tempCharacteristic->addDescriptor(new BLE2902());
+	tempDescriptor.setValue("Temperature -40-60°C");
+	tempCharacteristic->addDescriptor(&tempDescriptor);
+	tempCharacteristic->addDescriptor(new BLE2902());
 
-  pService->start();
+	pService->start();
 
-  // Start advertising
-  pServer->getAdvertising()->start();
+	// Start advertising
+	pServer->getAdvertising()->start();
 }
+
+Data *aD;
 
 void setup() {
 
-  InitBLE();
 
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+	aD = new Data();
+	InitBLE();
+
+	Serial.begin(115200);
+	Serial.println();
+	Serial.println();
 
 
-  u8g2.begin();
+	u8g2.begin();
 
-  if (!bmp.begin())
-  {
-	  Serial.println("Could not find BMP180 or BMP085 sensor at 0x77");
-	  while (1) {}
-  }
+	if (!bmp.begin())
+	{
+		Serial.println("Could not find BMP180 or BMP085 sensor at 0x77");
+		while (1) {}
+	}
 
 }
 
 void TransferBLE() {
-  //outHumidityCharacteristic.setValue(( uint8_t*) 2, 2);
-  //outHumidityCharacteristic.notify();
+	tempCharacteristic->setValue(aD->Temperature(), 2);
+	tempCharacteristic->notify();
 
-  BLEData *aD = new BLEData();
-
-  aD->FillData(bmp.readTemperature(), bmp.readPressure());
-
-  tempCharacteristic->setValue(aD->Temperature(), 2);
-  tempCharacteristic->notify();
-
-  pressureCharacteristic->setValue(aD->Pressure(), 4);
-  pressureCharacteristic->notify();
+	pressureCharacteristic->setValue(aD->Pressure(), 4);
+	pressureCharacteristic->notify();
 }
 
-float temperature;
-float pressure;
 
-void loop() {
+void loop()
+{
 
-	temperature = bmp.readTemperature();
-	pressure = bmp.readPressure();
+	aD->FillData(bmp.readTemperature(), bmp.readPressure());
 
 	u8g2.setFont(u8g2_font_5x7_tn);
 	u8g2.firstPage();
 	do {
 
 		u8g2.setCursor(0, 10);
-		u8g2.print(String(temperature));
+		u8g2.print(aD->getTemperatureAsString());
 		u8g2.setCursor(0, 20);
-		u8g2.print(String(pressure));
+		u8g2.print(aD->getPressureAsString());
 
 	} while (u8g2.nextPage());
-	delay(1000);
 
-  //TransferBLE();
-  //delay(4000);
-
-
-
+	TransferBLE();
+	delay(4000);
 }
 
 
